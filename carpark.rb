dc4cc83e83a0f7c2f802641e658f47d263c499c8
Piotr Zientara <314zientara@gmail.com>
2018-01-06 20:28:20 +0100

    refactores carpark

--- carpark.rb
+++ carpark.rb
@@ -1,45 +1,23 @@
 def escape(carpark)
   result = []
-
-  starting_floor = carpark[0]
-  start = starting_floor.find_index(2)
-  staircase = starting_floor.find_index(1)
-  unless staircase
-    if start + 1 == starting_floor.size
-      return result
-    else
-      return ["R#{starting_floor.size - start - 1}"]
-    end
-  end
-  if start > staircase
-    result << "L#{start - staircase}"
-  end
-  if start < staircase
-    result << "R#{staircase - start}"
-  end
-  current_index = staircase
-  down = 1
-  carpark.each_with_index do |floor, index|
-    next if index == 0
-    staircase = floor.find_index(1)
-    if staircase == current_index
+  down = 0
+  my_floor = carpark.find_index { |floor| floor.find_index(2) }
+  current_position = carpark[my_floor].find_index(2)
+  carpark.drop(my_floor).each do |floor|
+    case staircase ||= floor.find_index(1)
+    when nil
+      result << "D#{down}" if down > 0
+      last_move = floor.size - current_position - 1
+      result << "R#{last_move}" if last_move != 0
+    when current_position
       down += 1
       next
     else
-      if staircase
-        result << "D#{down}"
-        down = 1
-        if staircase > current_index
-          result << "R#{staircase - current_index}"
-        end
-        if staircase < current_index
-          result << "L#{current_index - staircase}"
-        end
-        current_index = staircase
-      else
-        result << "D#{down}"
-        result << "R#{floor.size - current_index - 1}" if (floor.size - current_index - 1) != 0
-      end
+      result << "D#{down}" if down > 0
+      down = 1
+      result << "R#{staircase - current_position}" if staircase > current_position
+      result << "L#{current_position - staircase}" if staircase < current_position
+      current_position = staircase
     end
   end
   result
